499d412383742e91c34b8535698556c778ae271b
Danil Merinov <danilmerinov9@gmail.com>
2025-04-18 00:35:51 -0400

    background color

--- src/lib.rs
+++ src/lib.rs
@@ -22,9 +22,10 @@ pub enum Color {
 
 
 impl Color {
-    ///  ANSI color code (foreground color)
-    fn fg_code(&self) -> u8 {
 
+    //  ANSI color code
+    // foreground color
+    fn fg_code(&self) -> u8 {
         match self {
             Color::Black => 30,
             Color::Red => 31,
@@ -43,6 +44,28 @@ impl Color {
             Color::BrightCyan => 96,
             Color::BrightWhite => 97,
         }
-        
+    }
+
+    // background color
+    fn bg_code(&self) -> {
+
+        match self {
+            Color::Black => 40,
+            Color::Red => 41,
+            Color::Green => 42,
+            Color::Yellow => 43,
+            Color::Blue => 44,
+            Color::Magenta => 45,
+            Color::Cyan => 46,
+            Color::White => 47,
+            Color::BrightBlack => 100,
+            Color::BrightRed => 101,
+            Color::BrightGreen => 102,
+            Color::BrightYellow => 103,
+            Color::BrightBlue => 104,
+            Color::BrightMagenta => 105,
+            Color::BrightCyan => 106,
+            Color::BrightWhite => 107,
+        }
     }
 }

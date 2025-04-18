a81eedbf04ec5ac641e0fcd466658d361510a9a7
Danil Merinov <danilmerinov9@gmail.com>
2025-04-17 22:43:42 -0400

    foreground color code func

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,48 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::fmt;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Color {
+    Black,
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    White,
+    BrightBlack,
+    BrightRed,
+    BrightGreen,
+    BrightYellow,
+    BrightBlue,
+    BrightMagenta,
+    BrightCyan,
+    BrightWhite,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Color {
+    ///  ANSI color code (foreground color)
+    fn fg_code(&self) -> u8 {
+
+        match self {
+            Color::Black => 30,
+            Color::Red => 31,
+            Color::Green => 32,
+            Color::Yellow => 33,
+            Color::Blue => 34,
+            Color::Magenta => 35,
+            Color::Cyan => 36,
+            Color::White => 37,
+            Color::BrightBlack => 90,
+            Color::BrightRed => 91,
+            Color::BrightGreen => 92,
+            Color::BrightYellow => 93,
+            Color::BrightBlue => 94,
+            Color::BrightMagenta => 95,
+            Color::BrightCyan => 96,
+            Color::BrightWhite => 97,
+        }
+        
     }
 }
